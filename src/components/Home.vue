9ebd6fd38e1e42e8de9c0af89ec66a9951fabf59
Slava Avramenok <_AVA@outlook.com>
2018-12-02 05:12:06 +0300

    Home page. Work in progress

---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <v-container>
+        <v-layout column align-center justify-center>
+            <v-flex xs5 md6 lg4>
+                <v-img
+                        :src="mainLogo"
+                        height="100%"
+                        width="550"
+                ></v-img>
+            </v-flex>
+            <v-flex>
+                <v-btn color="primary" large @click="uploadFile">
+                    Upload
+                    <v-icon right>backup</v-icon>
+                </v-btn>
+                <input id="uploader" ref="uploader" type="file"
+                       @сhange=""/>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        mainLogo: '',
+        created() {
+            this.mainLogo = require('@/assets/mainLogo.png')
+        },
+        methods: {
+            uploadFile() {
+                this.$refs.uploader.click();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #uploader {
+        display: none;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/components/Home'
+
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
-    routes: [],
+    routes: [
+        {
+            path: '/',
+            component: Home
+        }
+    ],
     mode: 'history'
 })
