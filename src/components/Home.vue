4ee9a2dcdf936d1bf574ac69c84e71aea09280f6
Slava Avramenok <_AVA@outlook.com>
2018-12-03 02:53:41 +0300

    Home page. Improved view. Keep working on drag&drop feature

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,20 @@
 <template>
-
-    <v-container>
+    <v-container id="drop-area" ref="dropArea">
         <v-layout column align-center justify-center>
-            <v-flex xs5 md6 lg4>
+            <div id="arrow-area">
+                <v-img
+                        :src="arrow"
+                        height="auto"
+                        max-width="80%"
+                ></v-img>
+                <span class="non-selectable">Try to drag & drop your file</span>
+            </div>
+            <v-flex xs4 md6 lg4>
                 <v-img
                         :src="mainLogo"
-                        height="100%"
-                        width="550"
+                        width="550px"
+                        max-width="100%"
+                        height="auto"
                 ></v-img>
             </v-flex>
             <v-flex>
@@ -25,13 +33,42 @@
     export default {
         name: "Home",
         mainLogo: '',
+        arrow: '',
         created() {
-            this.mainLogo = require('@/assets/mainLogo.png')
+            this.mainLogo = require('@/assets/mainLogo.png');
+            this.arrow = require('@/assets/arrow-right-hi.png');
+        },
+        mounted() {
+            const dropArea = this.$refs.dropArea;
+            const preventDefaults = (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+            };
+            ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+                dropArea.addEventListener(eventName, preventDefaults, false)
+            });
+            const highlight = (e) => {
+                dropArea.classList.add('highlight');
+                dropArea.firstChild.classList.add('disablePointerEvents');
+            };
+            const unhighlight = (e) => {
+                dropArea.classList.remove('highlight');
+                // dropArea.firstChild.classList.remove('disablePointerEvents');
+            };
+            ['dragenter', 'dragover'].forEach(eventName => {
+                dropArea.addEventListener(eventName, highlight, false)
+            });
+            ['dragleave', 'drop'].forEach(eventName => {
+                dropArea.addEventListener(eventName, unhighlight, false)
+            });
+
+
         },
         methods: {
             uploadFile() {
                 this.$refs.uploader.click();
             }
+
         }
     }
 </script>
@@ -40,4 +77,33 @@
     #uploader {
         display: none;
     }
+
+    #arrow-area {
+        position: absolute;
+        left: 5%;
+        top: 10%;
+        font-size: 16pt;
+    }
+
+    #drop-area {
+        position: relative;
+        height: 95%;
+    }
+
+    .highlight {
+        border: 5px dashed black;
+        border-radius: 20px;
+    }
+
+    .disablePointerEvents {
+        pointer-events: none;
+    }
+
+    .non-selectable {
+        -webkit-user-select: none; /* Safari */
+        -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* IE10+/Edge */
+        user-select: none; /* Standard */
+    }
+
 </style>
