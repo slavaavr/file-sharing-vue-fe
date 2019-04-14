c97c38db07387272e4210c64bb9aed14f15a52fa
Slava Avramenok <_AVA@outlook.com>
2019-04-15 02:54:01 +0300

    init of the vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import home from './home'
+import user from './user'
+import shared from './shared'
+import file from './file'
+import tagbox from './tagbox'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        home
+        home,
+        user,
+        shared,
+        file,
+        tagbox
     }
 })
