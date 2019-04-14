97dfd0c02e11e3531e08042f3877d84dbe198dc3
Slava Avramenok <_AVA@outlook.com>
2019-04-15 02:52:12 +0300

    basic logic for uploading files

--- src/components/home/HomeUpload.vue
+++ src/components/home/HomeUpload.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-container>
         <v-layout row align-center justify-center>
             <v-flex xs6 md6 lg6 xl4>
@@ -10,24 +10,39 @@
                         <v-flex>
                             <v-card>
                                 <v-card-title primary-title class="pb-0">
-                                    <div class="headline">Downloading...</div>
+                                    <div class="headline">{{fileName}}</div>
                                     <v-progress-linear
                                             color="info"
                                             height="15"
-                                            value="45"
+                                            :value="percentCompleted"
                                     ></v-progress-linear>
-                                    <p>456/512kb</p>
+                                    <p>{{loadedPartSize}}/{{totalFileSize}}b</p>
                                 </v-card-title>
-                                <v-card-actions class="pb-3">
-                                    <v-spacer></v-spacer>
-                                    <v-btn class="mr-2 error">Cancel</v-btn>
+                                <v-card-actions>
+                                    <v-alert
+                                            :value="isFileUploaded"
+                                            type="success"
+                                            transition="scale-transition"
+                                            style="width: 100%"
+                                    >
+                                        <v-layout row>
+                                            {{linkToFile}}
+                                            <v-spacer></v-spacer>
+                                            <v-tooltip v-model="showTooltip" top>
+                                                <template v-slot:activator="{}">
+                                                    <v-btn @click="copyToClipboard" @mouseleave="toggleTooltip">Copy</v-btn>
+                                                </template>
+                                                <span>Copied!</span>
+                                            </v-tooltip>
+                                        </v-layout>
+                                    </v-alert>
                                 </v-card-actions>
                             </v-card>
                         </v-flex>
                         <v-flex>
                             <v-card>
                                 <v-card-title>
-                                    <div class="headline">File name</div>
+                                    <div class="headline">Settings</div>
                                 </v-card-title>
                                 <v-divider light></v-divider>
                                 <v-layout row wrap align-center>
@@ -52,12 +67,12 @@
                                         <v-subheader>Tags</v-subheader>
                                     </v-flex>
                                     <v-flex xs6>
-                                       <app-tagbox></app-tagbox>
+                                        <app-tagbox></app-tagbox>
                                     </v-flex>
                                 </v-layout>
                                 <v-card-actions class="pa-3">
                                     <v-spacer></v-spacer>
-                                    <v-btn class="mr-2 info">Generate link</v-btn>
+                                    <v-btn class="mr-2 info" @click="sendFileOnServer">Generate link</v-btn>
                                 </v-card-actions>
                             </v-card>
                         </v-flex>
@@ -72,24 +87,59 @@
     import Tagbox from '@/components/Tagbox'
 
     export default {
-        props: {
-            changeHome: Function
-        },
         data() {
             return {
-                file: null,
                 storageTime: {title: '1 hour', time: 1},
                 variantsOfTime: [
                     {title: '1 hour', time: 1},
                     {title: '1 day', time: 24},
-                    {title: '5 days', time: 24*5},
-                    {title: '30 days', time: 24*30},
-                ]
+                    {title: '5 days', time: 24 * 5},
+                    {title: '30 days', time: 24 * 30},
+                ],
+                linkToFile: '',
+                isFileUploaded: false,
+                showTooltip: false,
+            }
+        },
+        watch: {
+            storageTime() {
+                this.$store.dispatch('setStorageTime', this.storageTime.title);
+            }
+        },
+        computed: {
+            percentCompleted() {
+                return this.$store.getters.loadedPartSize * 100 / this.$store.getters.totalFileSize
+            },
+            totalFileSize() {
+                return this.$store.getters.totalFileSize;
+            },
+            loadedPartSize() {
+                return this.$store.getters.loadedPartSize;
+            },
+            fileName() {
+                return this.$store.getters.file.name;
+            }
+        },
+        methods: {
+            sendFileOnServer() {
+                this.$store.dispatch('uploadFile')
+                    .then(link => {
+                        this.linkToFile = link;
+                        this.isFileUploaded = true;
+                    });
+            },
+            copyToClipboard() {
+                this.toggleTooltip();
+                navigator.clipboard.writeText(this.linkToFile);
+            },
+            toggleTooltip() {
+                this.showTooltip = !this.showTooltip
             }
         },
         components: {
             appTagbox: Tagbox
-        }
+        },
+
     }
 </script>
 
