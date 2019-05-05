7ac0210dadb4563292ff8bc6efe99e8ac4c5c14f
Slava Avramenok <_AVA@outlook.com>
2019-05-06 00:18:57 +0300

    stream component

--- src/components/Stream.vue
+++ src/components/Stream.vue
@@ -1,10 +1,41 @@
 <template>
-    <p>hello world!</p>
+    <v-layout>
+        <v-flex xs12 sm6 offset-sm3>
+            <v-card>
+                <v-subheader>Stream</v-subheader>
+                <v-container grid-list-md fluid class="pa-3">
+                    <v-layout row wrap>
+                        <v-flex
+                                v-for="file in files"
+                                :key="file.id"
+                                xs4
+                        >
+                            <app-file v-bind:data="file"></app-file>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
+    import File from '@/components/File'
+
     export default {
-        name: "stream"
+        name: "stream",
+        data() {
+            return {
+                files: null
+            }
+        },
+        components: {
+            appFile: File
+        },
+        created() {
+            this.$store.dispatch('generatePage')
+                .then(data => this.files = data);
+        },
     }
 </script>
 
